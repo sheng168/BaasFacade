2d343dfec7ea5e73728df6073f005ed6d1763261
Sheng Yu <sheng@jsy.us>
2012-10-26 14:39:55 -0400

    forgot the most important file junit

---
+++ ParseFacadeTest/src/us/jsy/android/shared/test/ParseFacadeTest.java
@@ -0,0 +1,81 @@
+package us.jsy.android.shared.test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import shared.parse.ParseFacade;
+
+import junit.framework.TestCase;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+public class ParseFacadeTest extends AndroidTestCase {
+	public void testParseNative() throws ParseException {
+		Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
+		Context context = this.getContext();
+		assertNotNull(context);
+		Parse.initialize(context, "xUodRIjJ4mnNJkBLK5N5YGxABdqRYEPx2eiO7X6n", "kB53FUFAW5G8OtABzzUJAJo4lzjiMJEGhBF9QLX7"); 
+
+		ParseObject to = new ParseObject("TestObject");
+		to.put("stringField", "name");
+		to.put("intField", 1987);
+		to.put("longField", 123456789012345L);
+		to.put("floatField", 1987.1f);
+		to.put("booleanField", true);
+		to.put("dateField", new Date());
+		to.put("byteField", new byte[4]);
+		to.put("listField", new ArrayList<Object>());
+		
+		to.save();
+
+		String id = to.getObjectId();
+		to.put("objectId", id);
+
+		ParseQuery q = new ParseQuery("TestObject");
+		ParseObject po = q.get(id);
+		int int1 = po.getInt("longField");
+		long long1 = po.getLong("longField_");
+		byte[] bytes = po.getBytes("byteField");
+		
+		System.out.printf("%d %d %d", int1, long1, bytes.length);
+	}
+	
+	public void testFacade() throws ParseException {
+		ParseFacade<TestObject> pof = ParseFacade.get(TestObject.class);
+		TestObject to = pof.create();
+		
+		String name = "type-safe";
+		to.stringField(name);
+		to.floatField(1987.1015f);
+		to.intField(1987);
+		to.longField(1234567890123456L);
+		to.byteField(new byte[9]);
+		
+		ParseObject po = to.parseObject();
+		po.save();
+		String id = po.getObjectId();
+		
+		ParseObject po2 = pof.query().parseQuery().get(id);
+		TestObject to2 = pof.wrap(po2);
+		
+		String n = to2.stringField();
+		assertEquals(name, n);
+		int int1 = to2.intField();
+		float float1 = to2.floatField();
+		Long long1 = to2.longField();
+//		Date createdAt = to2.createdAt();
+//		Date updatedAt = to2.updatedAt();
+		to2.byteField();
+		
+		assertNotNull(id);
+//		to2.objectId(id);
+		
+//		System.out.printf("%d %f %d", int1, float1, long1);
+	}
+}
