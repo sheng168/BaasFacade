077c7596c24f071cd90cf52ed0ba050a62719c07
Sheng Yu <sheng@jsy.us>
2012-10-26 09:19:56 -0400

    android library project

---
+++ ParseFacade/src/shared/parse/ParseBase.java
@@ -0,0 +1,28 @@
+package shared.parse;
+
+import com.parse.ParseObject;
+
+/**
+ * Extend this interface to define you ParseObject class field.
+ * Don't use primitive types if you may be reading fields with null values.
+ * Supported types include boolean, int, long, double (not float), String, Date.
+ * If you never write to a field(even a primitive one) it'll have a null value rather than a default value.
+ * Don't try to read the built-in fields(objectId, createdAt, updatedAt) through this interface.
+ * @author sheng
+ *
+ */
+public interface ParseBase {
+	static final String PARSE_OBJECT = "parseObject";
+	
+	/**
+	 * 
+	 * @return the backing object
+	 */
+	ParseObject parseObject();
+	
+//	String objectId();
+//	void objectId(String in);
+//	
+//	Date createdAt();
+//	Date updatedAt();
+}
---
+++ ParseFacade/src/shared/parse/ParseFacade.java
@@ -0,0 +1,220 @@
+package shared.parse;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+/**
+ * Type safe facade for the parse.com API.  
+ * Class and field names are defined using an interface instead of String(s).
+ * You should only create one instance for each interface class.
+ * 
+ * @author sheng
+ *
+ * @param <T>
+ */
+public class ParseFacade<T> {
+	Class<T> clazz;
+	Class<?>[] interfaces;
+
+	private ParseFacade(Class<T> clazz) {
+		super();
+		assert clazz != null;
+		assert clazz.isInterface();
+		
+		this.clazz = clazz;
+		Class<?>[] interfaces = {clazz};
+		this.interfaces = interfaces;
+	}
+
+	/**
+	 * Use this to get instances for related to each class.
+	 * 
+	 * @param clazz interface that extends ParseBase
+	 * @return may be a cache or new instance
+	 */
+	public static <T> ParseFacade<T> get(Class<T> clazz) {
+		return new ParseFacade<T>(clazz);
+	}
+	
+	/**
+	 * 
+	 * @return a new instance of clazz
+	 */
+	public T create() {
+		ParseObject po = new ParseObject(clazz.getSimpleName());
+		return wrap(po);
+	}
+
+	@SuppressWarnings("unchecked")
+	public T wrap(ParseObject po) {
+		assert po != null;
+		assert clazz.getSimpleName().equals(po.getClassName());
+		
+		Object obj = Proxy.newProxyInstance(clazz.getClassLoader(), interfaces, 
+				new ParseObjectHandler(po));
+		return (T)obj;
+	}
+
+	public List<T> wrap(List<ParseObject> l) {
+		ArrayList<T> r = new ArrayList<T>();
+		for (ParseObject parseObject : l) {
+			r.add(wrap(parseObject));
+		}
+		return r;
+	}
+	
+	public Query<T> query() {
+//		new ParseQuery(RegionUser.class.getSimpleName())
+//		.whereEqualTo("name", name)
+//		.whereEqualTo("user", ParseUser.getCurrentUser())
+//		.
+
+		return new Query<T>(this);
+	}
+	
+	public static class Query<T> {
+		ParseFacade<T> facade;
+		ParseQuery pq;
+		
+		public Query(ParseFacade<T> parseFacade) {
+			this.facade = parseFacade;
+			pq = new ParseQuery(parseFacade.clazz.getSimpleName());
+		}
+
+		@SuppressWarnings("unchecked")
+		public T equalTo() {
+			return (T) Proxy.newProxyInstance(facade.clazz.getClassLoader(), facade.interfaces, new ParseQueryEqualHandler(pq));
+		}
+
+		@SuppressWarnings("unchecked")
+		public T orderAsc() {
+			return (T) Proxy.newProxyInstance(facade.clazz.getClassLoader(), facade.interfaces, new ParseQueryOrderAscHandler(pq));
+		}
+
+		@SuppressWarnings("unchecked")
+		public T orderDesc() {
+			return (T) Proxy.newProxyInstance(facade.clazz.getClassLoader(), facade.interfaces, new ParseQueryOrderDescHandler(pq));
+		}
+		
+		public ParseQuery parseQuery() {
+			return pq;
+		}
+	}
+	
+	static class ParseObjectHandler implements InvocationHandler {
+		ParseObject obj;
+
+		public ParseObjectHandler(ParseObject obj) {
+			this.obj = obj;
+		}
+
+		public Object invoke(Object proxy, Method m, Object[] args)
+				throws Throwable {
+//			try {
+				String name = m.getName();
+				String key = name; //.substring(3); // assume get set
+				if (args.length == 0) {
+					if (ParseBase.PARSE_OBJECT.equals(name))
+						return obj;
+					String typeName = m.getReturnType().getName();
+					if (typeName.equalsIgnoreCase("float")) {
+						return (float)obj.getDouble(key);
+					} else {
+						Object object = obj.get(key);
+						if (m.getReturnType().isPrimitive() && object == null) {
+							return 0;
+						} else {
+							return object;
+						}
+					}
+				} else {
+					Object object = args[0];
+					if (key.equals("objectId"))
+						System.out.println("value " + object);
+					obj.put(key, object);
+					return null;
+				}
+//			} catch (InvocationTargetException e) {
+//				throw e.getTargetException();
+//			} catch (Exception e) {
+//				throw e;
+//			}
+			// return something
+		}
+	}
+	
+	private static class ParseQueryEqualHandler implements InvocationHandler {
+		ParseQuery pq;
+		
+		@Override
+		public Object invoke(Object proxy, Method m, Object[] args)
+				throws Throwable {
+			String name = m.getName();
+			if (args.length == 0) {
+				throw new UnsupportedOperationException("Can only set values, not read from query");
+			} else {
+				pq.whereEqualTo(name, args[0]);
+				return null;
+			}
+		}
+
+		public ParseQueryEqualHandler(ParseQuery pq) {
+			super();
+			this.pq = pq;
+		}
+		
+	}
+
+	private static abstract class ParseQueryOrderHandler implements InvocationHandler {
+		ParseQuery pq;
+		
+		@Override
+		public Object invoke(Object proxy, Method m, Object[] args)
+				throws Throwable {
+			String name = m.getName();
+			if (args.length == 0) {
+				orderBy(name);
+				return null;
+			} else {
+				throw new UnsupportedOperationException("Can only read values");
+			}
+		}
+
+		abstract void orderBy(String name);
+
+		public ParseQueryOrderHandler(ParseQuery pq) {
+			this.pq = pq;
+		}
+		
+	}
+	
+	private static class ParseQueryOrderDescHandler extends ParseQueryOrderHandler {
+		public ParseQueryOrderDescHandler(ParseQuery pq) {
+			super(pq);
+		}
+
+		@Override
+		void orderBy(String name) {
+			pq.orderByDescending(name);
+		}
+
+	}
+
+	private static class ParseQueryOrderAscHandler extends ParseQueryOrderHandler {
+		public ParseQueryOrderAscHandler(ParseQuery pq) {
+			super(pq);
+		}
+
+		@Override
+		void orderBy(String name) {
+			pq.orderByAscending(name);
+		}
+
+	}
+}
